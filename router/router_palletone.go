caf3f7e6d1fa8fde266641fb374e80813b3bf52a
eric-greencomb <ministor@126.com>
2019-07-10 16:19:36 +0800

    c.

--- router/router_palletone.go
+++ router/router_palletone.go
@@ -13,7 +13,7 @@ func SetupPalletoneRouter(g *gin.Engine) {
 		rpalletone.POST("tx/raw", handler.GetRawTx)
 
 		// 根据参数，生成交易结构，并进行rlp编码
-		// rpalletone.POST("tx/encoding", handler.GetRawTxEncoding)
+		rpalletone.POST("tx/encoding", handler.GetRawTxEncoding)
 
 		// 对参数进行rlp解码
 		// rpalletone.POST("tx/decoding", handler.GetRawTxDecoding)
